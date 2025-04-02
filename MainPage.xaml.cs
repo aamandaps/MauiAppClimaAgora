67bd13262c78be19e0c6875101d1e64c8bb68b80
aamandaps <aamandaps01@gmail.com>
2025-04-02 16:45:30 -0300

    Funcionalidades básicas

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace MauiAppClimaAgora
+﻿using MauiAppClimaAgora.Models;
+using MauiAppClimaAgora.Services;
+
+
+namespace MauiAppClimaAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -11,14 +15,50 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+          
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                { 
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        lbl_resp.Text = dados_previsao;
+
+                        dados_previsao = $"Descrição: " +
+                                         $"Latitude: t.{lat} \n" +
+                                         $"Longitude: t.{lon} \n" +
+                                         $"Visibilidade:t.{visibility} \n " +
+                                         $"Temperatura mínima:  t.{temp_min} \n" +
+                                         $"Temperatura máxima: t.{temp_max} \n" +
+                                         $"Velocidade do vento: t.{speed} \n" +
+                                         $"Nascer do sol: t.{sunrise} \n" +
+                                         $"Pôr do sol: t.{sunset} \n";
+                    }
+                    else 
+                    {
+                        lbl_resp.Text = "Sem dados de previsão";
+                    }
+
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha o campo cidade";
+                }
+                    
+
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
         }
     }
 
